296d62ab31301c1fe538d63599c267207185085d
MaciejMalicki <robert8822@wp.pl>
2017-01-18 22:08:43 +0100

    pętle

---
+++ witam.py
@@ -0,0 +1,19 @@
+
+lista = []
+for x in range(1,101):
+	lista.append(x)
+	pass
+print (lista)
+
+lista2 = []
+for y in lista:
+	lista2.append(y*5)
+
+	pass
+print(lista2)
+
+lista3 = []
+for z in lista2:
+	lista3.append(z-50)
+	pass
+print(lista3)
