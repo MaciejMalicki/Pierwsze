6f630cc5e45a2f3e8028bd8740c4af5980fc8a53
MaciejMalicki <robert8822@wp.pl>
2017-02-14 17:34:59 +0100

    trening

---
+++ I zadanie.py
@@ -0,0 +1,9 @@
+suma = 0 
+for x in range(1,1000):
+	if x%3 == 0 or x%5 ==0:
+		suma = suma + x
+		pass
+	pass
+print(suma)
+teskt= "kupa"
+print(teskt[1:3:1]) 
---
+++ funkcje.py
@@ -0,0 +1,4 @@
+def jakas_funckja(liczba):
+	for cyfra in str(liczba):
+		print(cyfra)
+	pass
